38d56bcb05df912c31817f01c2a44da99d80488b
darkinowls <BlackInOwls@gmail.com>
2022-09-13 13:44:51 +0300

    angular material part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,12 @@ import { AppComponent } from './app.component';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component'
 import { MatListModule } from "@angular/material/list";
+import { MatGridListModule } from '@angular/material/grid-list'
+import { MatCardModule } from '@angular/material/card'
+import { MatButtonModule } from '@angular/material/button'
+
+
+
 
 
 @NgModule({
@@ -20,6 +26,9 @@ import { MatListModule } from "@angular/material/list";
     MatToolbarModule,
     FlexLayoutModule,
     MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
