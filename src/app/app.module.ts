78d8bf833442e0596200d9dfbc2cbcee8c615ba1
darkinowls <BlackInOwls@gmail.com>
2022-09-17 14:53:40 +0300

    static folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,8 +12,8 @@ import { MatGridListModule } from '@angular/material/grid-list'
 import { MatCardModule } from '@angular/material/card'
 import { MatButtonModule } from '@angular/material/button';
 import { DishdetailComponent } from './components/dishdetail/dishdetail.component'
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './static/header/header.component';
+import { FooterComponent } from './static/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
